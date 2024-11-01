be47b772633c1756c0a399978957d01df52ce489
Mubariz Nacafov <mubariz.necefov.17@gmail.com>
2024-11-01 23:19:43 +0400

    adding hero page to site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "../scss/pages/hero/_hero.scss";
-
+import PersonalPhoto from "../assets/person_photo.jpg";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="left_side">salam</div>
-      <div className="right_side">necesiniz</div>
+      <div className="left_side">
+        <h3>Aveliable for work</h3>
+        <h1>Building Scalable Modern Websites for the Future</h1>
+        <div className="buttons">
+          <div className="download_btn">
+            <button>Download CV</button>
+          </div>
+          <div className="scroll_down_btn">
+            <button>Scroll down</button>
+          </div>
+        </div>
+      </div>
+      <div className="right_side">
+        <div className="img">
+          <img src={PersonalPhoto} alt="person_photo" />
+        </div>
+      </div>
     </div>
   );
 };
