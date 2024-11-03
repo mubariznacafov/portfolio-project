e03720139c68d933f1364c1cde6ba00ebf2e751b
Mubariz Nacafov <mubariz.necefov.17@gmail.com>
2024-11-04 00:39:43 +0400

    fixed move cards to smooth move cards

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -97,6 +97,98 @@ const Customers = () => {
           </div>
         </div>
       </div>
+      <div className="cards">
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
+            architecto?
+          </p>
+          <div className="person">
+            <div className="img">
+              <img src={Person1} alt="person_1" />
+            </div>
+            <div className="personal_info">
+              <h3>Ethan Caldwell</h3>
+              <h2>NexaWave</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
