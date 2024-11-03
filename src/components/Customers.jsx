b6a1c56ba83bd43a0290b2a1bb65e011fd931a37
Mubariz Nacafov <mubariz.necefov.17@gmail.com>
2024-11-04 02:09:22 +0400

    styling cards

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "../scss/pages/customers/_customers.scss";
 import Person1 from "../assets/people-1.jpg";
+import Person2 from "../assets/people-2.jpg";
+import Person3 from "../assets/people-3.jpg";
+import Person4 from "../assets/people-4.jpg";
+import Person5 from "../assets/people-5.jpg";
+import Person6 from "../assets/people-6.jpg";
 
 const Customers = () => {
   return (
@@ -8,91 +13,91 @@ const Customers = () => {
       <div className="cards">
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Absolutely loved the layout! Very clean and easy to navigate. I can
+            tell a lot of thought went into every detail."
           </p>
           <div className="person">
             <div className="img">
               <img src={Person1} alt="person_1" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Amelia Foster</h3>
+              <h2>BrightCore Designs</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Your portfolio is impressive! Each project really highlights your
+            skills. I especially liked the animations."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person2} alt="person_2" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Lucas Thompson</h3>
+              <h2>InnovateX</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "A fantastic showcase of talent! The responsiveness is seamless, and
+            the colors are well-chosen."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person3} alt="person_3" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Maria Lopez</h3>
+              <h2>Peak Solutions</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person4} alt="person_4" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Rauf Najaf</h3>
+              <h2>GuestPet</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person5} alt="person_5" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Oliver Chen</h3>
+              <h2>UnityLabs</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person6} alt="person_6" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Azer Ahadli</h3>
+              <h2>HYPE+</h2>
             </div>
           </div>
         </div>
@@ -100,91 +105,91 @@ const Customers = () => {
       <div className="cards">
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Absolutely loved the layout! Very clean and easy to navigate. I can
+            tell a lot of thought went into every detail."
           </p>
           <div className="person">
             <div className="img">
               <img src={Person1} alt="person_1" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Amelia Foster</h3>
+              <h2>BrightCore Designs</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Your portfolio is impressive! Each project really highlights your
+            skills. I especially liked the animations."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person2} alt="person_2" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Lucas Thompson</h3>
+              <h2>InnovateX</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "A fantastic showcase of talent! The responsiveness is seamless, and
+            the colors are well-chosen."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person3} alt="person_3" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Maria Lopez</h3>
+              <h2>Peak Solutions</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person4} alt="person_4" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Rauf Najaf</h3>
+              <h2>GuestPet</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person5} alt="person_5" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Oliver Chen</h3>
+              <h2>UnityLabs</h2>
             </div>
           </div>
         </div>
         <div className="card">
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem,
-            architecto?
+            "Very user-friendly and visually appealing. This is a great example
+            of modern web design."
           </p>
           <div className="person">
             <div className="img">
-              <img src={Person1} alt="person_1" />
+              <img src={Person6} alt="person_6" />
             </div>
             <div className="personal_info">
-              <h3>Ethan Caldwell</h3>
-              <h2>NexaWave</h2>
+              <h3>Azer Ahadli</h3>
+              <h2>HYPE+</h2>
             </div>
           </div>
         </div>
